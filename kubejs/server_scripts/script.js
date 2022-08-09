99bea1c748835950c07767cc1f79c80b05e409db
MHYC133 <89076764+MHYCWasTaken@users.noreply.github.com>
2022-08-09 15:58:55 +0800

    安山岩合金配方更改

--- kubejs/server_scripts/script.js
+++ kubejs/server_scripts/script.js
@@ -8,7 +8,26 @@ settings.logErroringRecipes = true
 console.info('Hello, World! (You will see this line every time server resources reload)')
 
 onEvent('recipes', event => {
-	// Change recipes here
+	
+	// remove all recipes to andesite alloy
+	event.remove({output: 'create:andesite_alloy'})
+	// add a shapeless recipe to andesite alloy
+	//event.shapeless('4x create:andesite_alloy', ['minecraft:andesite', 'tconstruct:seared_brick'])
+	event.shaped( '2x create:andesite_alloy', [
+		'AB',
+		'BA'
+	],{
+		A: 'minecraft:andesite',
+		B: 'tconstruct:seared_brick'
+	})
+	event.shaped( '2x create:andesite_alloy', [
+		'BA',
+		'AB'
+	],{
+		A: 'minecraft:andesite',
+		B: 'tconstruct:seared_brick'
+	})
+
 })
 
 onEvent('item.tags', event => {
